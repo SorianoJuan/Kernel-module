696eaed9e7799dab9bd59bca323c26914c82b944
Torce <ju_sorianoo88@hotmail.com>
2017-11-25 21:08:35 -0300

    Initial commit

---
+++ encrypter/.encrypter.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/torce/Desktop/encrypter/encrypter.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/torce/Desktop/encrypter/encrypter.ko /home/torce/Desktop/encrypter/encrypter.o /home/torce/Desktop/encrypter/encrypter.mod.o ;  true
---
+++ encrypter/.tmp_versions/encrypter.mod
@@ -0,0 +1,3 @@
+/home/torce/Desktop/encrypter/encrypter.ko
+/home/torce/Desktop/encrypter/encrypter.o
+
